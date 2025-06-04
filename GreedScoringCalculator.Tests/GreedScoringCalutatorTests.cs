55f65c5fdfb69113d88673d3472e521620365999
johnb08 <johnb08.kodehode@gmail.com>
2025-06-04 10:35:06 +0200

    tests

--- GreedScoringCalculator.Tests/GreedScoringCalutatorTests.cs
+++ GreedScoringCalculator.Tests/GreedScoringCalutatorTests.cs
@@ -1,10 +1,42 @@
-namespace GreedScroringCalculator.Tests;
+namespace GreedScoringCalculator.Tests;
 
-public class UnitTest1
+public class GreedScoringCalculatorTests
 {
     [Fact]
-    public void Test1()
+    public void TestIfCalculatorCanInitialize()
     {
+        var scoreCalculator = new GreedScoringCalculator();
 
+        Assert.NotNull(scoreCalculator);
     }
+
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void TestIfScoreIsCorrect(int[] scores, int expected)
+    {
+        var calculator = new GreedScoringCalculator();
+        var scores = calculator.Calculate(scores);
+
+        Assert.Equal(expected, scores);
+    }
+
+    public static TheoryData<object[]> Data =>
+    [
+        [
+            new int[]{5, 1, 3, 4, 1 },
+            250
+        ],
+        [
+            new int[] { 1,1,1,3,1},
+            1100
+        ],
+        [
+            new int[]{2,4,4,5,4},
+            450
+        ],
+        [
+            new int[]{ 5,5,3,5,1},
+            600
+        ]
+    ];
 }
