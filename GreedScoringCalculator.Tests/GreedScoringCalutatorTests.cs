c17eb69a3f52fad961e8ef6bc4598fe46877ab0f
johnb08 <johnb08.kodehode@gmail.com>
2025-06-04 10:50:12 +0200

    squigglies

--- GreedScoringCalculator.Tests/GreedScoringCalutatorTests.cs
+++ GreedScoringCalculator.Tests/GreedScoringCalutatorTests.cs
@@ -12,7 +12,7 @@ public class GreedScoringCalculatorTests
     }
 
     [Theory]
-    [MemberData(nameof(Data))]
+    [MemberData(nameof(Data), MemberType = typeof(GreedScoringCalculatorTests))]
     public void TestIfScoreIsCorrect(int[] scores, int expected)
     {
         var calculator = new GreedScoringCalculator();
@@ -21,23 +21,23 @@ public class GreedScoringCalculatorTests
         Assert.Equal(expected, score);
     }
 
-    public static TheoryData<object[]> Data =>
-    [
-        [
+    public static TheoryData<int[], int> Data =>
+    new TheoryData<int[], int>{
+        {
             new int[]{5, 1, 3, 4, 1 },
             250
-        ],
-        [
+        },
+        {
             new int[] { 1,1,1,3,1},
             1100
-        ],
-        [
+        },
+        {
             new int[]{2,4,4,5,4},
             450
-        ],
-        [
+        },
+        {
             new int[]{ 5,5,3,5,1},
             600
-        ]
-    ];
+        }
+    };
 }
